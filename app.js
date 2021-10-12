66a2e9cffba561ed7d8a9ac844828f6046c82683
Trevor Mwangi <trevorzac16@yahoo.com>
2021-10-12 05:46:11 -0400

    get guess method

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
   }
+  ,getGuess: function(guess1,guess2){
+    return (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`);
+   }
 }
